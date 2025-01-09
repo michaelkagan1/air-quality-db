0f64b115e593728fdb0986e568b31e6a0252985c
mkagan <michaelkagan1994@gmail.com>
2025-01-09 15:17:47 -0800

    made ETL.py leaner, made df_to_db helper func to break up large main() func. Added .gitignore

--- ETL.py
+++ ETL.py
@@ -33,8 +33,7 @@ def main():
 
 	logger.info('%s: Starting API requests.', datetime.now().ctime())
 	
-	#define date ranges for getting aqi data
-	#date_to is todays date.
+	#define date ranges for getting aqi data: date_to is todays date.
 	date_to = date.today().isoformat()
 
 	#date_from is the most recent (or max) date from the datetime column. Returns as datetime object
@@ -42,11 +41,10 @@ def main():
 	date_from = curs.fetchone()[0]
 	date_from = date_from.date().isoformat()
 
-	print(f'Fetching AQI data from {date_from} to {date_to}...\n')
 	logger.info(f'Fetching AQI data from {date_from} to {date_to}.')
 
 	for loc_id in location_ids:
-		#call get_loc to send location endpoint request and return json object of response
+		#send location endpoint request and return json object of response
 		json_loc = get_location_response(loc_id)
 		
 		if json_loc is None:
@@ -57,6 +55,7 @@ def main():
 		#unpack dataframes from dfs
 		locations_df, countries_df, sensors_df, elements_df = dfs
 
+		#get dataframe of all sensor aqi data at location at loc_id
 		aqi_df = multi_aqi_request_to_df(sensor_ids, loc_id, date_from, date_to)
 
 		if aqi_df.empty:	
@@ -66,35 +65,43 @@ def main():
 		tables = ['aqi', 'locations', 'countries', 'elements', 'sensors']	#table names in SQL 
 		dataframes = [aqi_df, locations_df, countries_df, elements_df, sensors_df]	#dataframes in the same order
 
-
-		for table, df in zip(tables, dataframes):	#zip so each table and source dataframe can be associated with eachother. 
-
-			# extract column headers from dataframe
-			head = df.columns.to_list()
-
-			#make string of '%s' elements for each value that will be inserted in each row. One per column in a dataframe. Use # of elements in the header list.
-			placeholder = ', '.join(['%s']*len(head))
-
-			#convert header list to a tuple, all in a string. also change single quotes to backticks.
-			head = str(tuple(head)).replace("'","`")
-
-			#Execute query: 1) insert table name, column headers string, and %s placeholder string (for prepared statement format)
-			#IGNORE used to ignore duplicate entries
-			query = "INSERT IGNORE INTO `{}` {} VALUES ({})".format(table, head, placeholder) 
-
-			#row by row, change list into tuple, to plug into 'insert many' method. List of tuples is argument for insert_many. Each element in list is a separate set of values to be inserted. 
-			values = [(tuple(row)) for row in df.values] 
-
-			#Try inserting into each table, print error on fail and keep looping
-			try:
-				curs.executemany(query, values)
-			except Exception as e:
-				logger.warning('Table insert unsuccessfull: %s', e)
-				continue
+		for tablename, df in zip(tables, dataframes):	#zip so each table and source dataframe can be associated with eachother. 
+			#insert to all 5 tables in db
+			insert_df_to_db(tablename, df)
 
 		#commit changes to sql. (like save)
 		cnx.commit()
 	
+	return
+
+#helper function for inserting a df to associated table in aqi database 
+def insert_df_to_db(tablename, df):
+	# extract column headers from dataframe
+	head = df.columns.to_list()
+
+	#make string of '%s' elements for each value that will be inserted in each row. One per column in a dataframe. Use # of elements in the header list.
+	placeholder = ', '.join(['%s']*len(head))
+
+	#convert header list to a tuple, all in a string. also change single quotes to backticks.
+	head = str(tuple(head)).replace("'","`")
+
+	#Execute query: 1) insert table name, column headers string, and %s placeholder string (for prepared statement format)
+	#IGNORE used to ignore duplicate entries
+	query = "INSERT IGNORE INTO `{}` {} VALUES ({})".format(tablename, head, placeholder) 
+
+	#row by row, change list into tuple, to plug into 'insert many' method. List of tuples is argument for insert_many. Each element in list is a separate set of values to be inserted. 
+	values = [(tuple(row)) for row in df.values] 
+
+	#Try inserting into each table, print error on fail and keep looping
+	try:
+		curs.executemany(query, values)
+	except Exception as e:
+		logger.warning('Table insert unsuccessfull: %s', e)
+		continue
+
+
+
+
 #retrieves ids of all target locations from 'locations list.csv' file
 def location_ids_from_file(filename):
         #open file in read mode, read line to list object and return list
