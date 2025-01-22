b045a01242c0d101f0623bb2bc26bcb13683869a
mkagan <michaelkagan1994@gmail.com>
2025-01-22 15:45:41 -0800

    added plotly functionality, successfully graphed multi-choice countries PM2.5 trends

--- stream.py
+++ stream.py
@@ -1,71 +1,66 @@
 from connectdb import connect_db
 from matplotlib import pyplot as plt
+import plotly.express as px
+import numpy as np
 import pandas as pd
 import streamlit as st
+from pathlib import Path
 
 def main():
+    #define path
+    path = Path().cwd()
+
+    #create layout for streamlit page
+#    st.image(path/'sky.jpg')
+    st.image('sky.jpg')
+    st.title("Air Quality Index in Capital Cities Around the World")
+    st.write('')
 
     # Establish connection and cursor with database as IAM user
     cnx, curs = connect_db()
     
     # query desired data from aqi as a pandas dataframe
     query_str = query()
-    df = query_to_df(curs, query_str)
 
-    # transform data # limit to top 5 and bottom 5 countries
-    dftop5 = df.iloc[:5] 
-    dfbot5 = df.iloc[-5:] 
-    df2 = pd.concat([dftop5, dfbot5], axis=0)
-   # import pdb; pdb.set_trace()
+    @st.cache_data
+    def query_to_df(query):
+        return pd.read_sql_query(query, cnx)
+        
+    df = query_to_df(query())
+    df2 = df.copy()
     
-    stream(df2.drop(columns = 'no. measurements'))
+    #display raw data on page
+    st.write('Raw data')
+    st.dataframe(df2)
 
-    # make graphs
-    plt.bar(df2['name'], df2['avg pm2.5'])
-    plt.xlabel('Country')
-    plt.ylabel('Average PM2.5')
-    plt.tick_params(axis='x', rotation=45)
-    plt.grid(axis='y', zorder=0)    # set grid to be behind bars
-    plt.title('Top and Bottom 5 Avg PM 2.5 by Country (Capital City)')
-    plt.tight_layout()
-    plt.show()
+    #give option bar for countries, taken from df
+    countries = df2['country'].sort_values().unique()
+    selected = st.multiselect('Select a country', countries)
 
+    #filter out countries selected and sort by country and date so lines don't spaghetti
+    df2 = df2[df2.country.isin(selected)]
+    df2.sort_values(by=['country', 'datetime'], inplace=True)
+    df2[df2['pm2.5'] < 0 ] = np.nan
+    
+
+    # plotly instead of pyplot
+    fig = px.line(df2, x='datetime', y='pm2.5', color = 'country')
+
+    #TODO: figure out why no lines are plotting
+    st.plotly_chart(fig, color='country')
+
+
+#TODO: use pythonic library like sqlalchemy
 def query():
+    #element id 2 is PM2.5
     query = """
-        SELECT name, ROUND(AVG(value),2) AS 'avg_pm2.5', COUNT(value) AS 'number measurements'
+        SELECT datetime, countries.name AS country, elements.name AS pollutant, value
         FROM countries 
         JOIN locations ON countries.id = locations.country_id
         JOIN aqi ON locations.id = aqi.location_id
-        WHERE element_id = 2
-        GROUP BY country_id
-        HAVING `avg_pm2.5` > 0
-        ORDER BY `avg_pm2.5` DESC
+        JOIN elements on aqi.element_id = elements.id
             """
     return query
-
-def query_to_df(curs, query):
-    #define query
-       #execute in database
-    curs.execute(query)
-
-    #retrieve results and clear cursor
-    data = curs.fetchall()
-
-    #convert data to df
-    df = pd.DataFrame(data, columns=['name', 'avg pm2.5', 'no. measurements'])
-    return df
-    
-
-def stream(dataframe):
-    # establish streamlit app
-    if st.button('click me'):
-        st.bar_chart(dataframe,
-                     x='name',
-                     y='avg pm2.5')
-                     
-
-
-    # stream with streamlit
 
 
 if __name__ == '__main__':
